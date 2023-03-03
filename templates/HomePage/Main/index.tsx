e3dd3f725915593d11a2f01226fee0fdb397d26e
Joshua Mathew <jm@Joshuas-iMac.local>
2023-03-03 18:45:34 +0530

    title edit

--- templates/HomePage/Main/index.tsx
+++ templates/HomePage/Main/index.tsx
@@ -18,49 +18,50 @@ type MainProps = {
 };
 
 const Main = ({ scrollToRef }: MainProps) => (
-    <div className={cn("section", styles.section)}>
-        <div className={cn("container", styles.container)}>
-            <div className={styles.head}>
-                <div className={styles.stage}>INTRODUCING TESSERACT</div>
-                <div className={cn("h1", styles.title)}>
-                    Decentralized Infrastructure for Digital Experiences
-                </div>
-                <div className={styles.info}>
-                Tesseract Protocol is a player-owned, permissionless infrastructure that anyone can use to build web3 games
-                </div>
-            </div>
-            <div className={styles.images}>
-                <div className={styles.image}>
-                    <Image
-                        src="/images/lines.svg"
-                        width={1578}
-                        height={1585}
-                        alt="Lines"
-                    />
-                </div>
-                {images.map((image, index) => (
-                    <Parallax
-                        className={styles.image}
-                        speed={1}
-                        easing="easeInQuad"
-                        rotate={index === 2 ? [2, -10] : [-4, 15]}
-                        key={index}
-                    >
-                        <Image src={image} layout="fill" alt="Figure" />
-                    </Parallax>
-                ))}
-            </div>
-            <Scroll
-                className={styles.scroll}
-                title="SCROLL TO EXPLORE"
-                onScroll={() =>
-                    scrollToRef.current.scrollIntoView({
-                        behavior: "smooth",
-                    })
-                }
-            />
-        </div>
-    </div>
-);
+	<div className={cn('section', styles.section)}>
+		<div className={cn('container', styles.container)}>
+			<div className={styles.head}>
+				<div className={styles.stage}>INTRODUCING TESSERACT</div>
+				<div className={cn('h1', styles.title)}>
+					Decentralized Infrastructure for Digital Experiences
+				</div>
+				<div className={styles.info}>
+					Tesseract Protocol is a permissionless decentralized player
+					infrastructure for web3 experiences
+				</div>
+			</div>
+			<div className={styles.images}>
+				<div className={styles.image}>
+					<Image
+						src='/images/lines.svg'
+						width={1578}
+						height={1585}
+						alt='Lines'
+					/>
+				</div>
+				{images.map((image, index) => (
+					<Parallax
+						className={styles.image}
+						speed={1}
+						easing='easeInQuad'
+						rotate={index === 2 ? [2, -10] : [-4, 15]}
+						key={index}
+					>
+						<Image src={image} layout='fill' alt='Figure' />
+					</Parallax>
+				))}
+			</div>
+			<Scroll
+				className={styles.scroll}
+				title='SCROLL TO EXPLORE'
+				onScroll={() =>
+					scrollToRef.current.scrollIntoView({
+						behavior: 'smooth',
+					})
+				}
+			/>
+		</div>
+	</div>
+)
 
 export default Main;
